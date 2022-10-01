ab9106235f599d84edbea03c356f9254b622ba08
loggerboy9325 <loggerboy9325@gmail.com>
2022-10-01 17:53:38 -0400

    testing

--- s3_files/script.js
+++ s3_files/script.js
@@ -1,5 +1,5 @@
 const API_URL =
-  "https://dv61tts9th.execute-api.us-east-1.amazonaws.com/test/visitorcounter";
+  "https://bupv7vssqj.execute-api.us-east-1.amazonaws.com/example/mydemoresource";
 
 async function getvisitors() {
   const response = await fetch(API_URL);
