9e974e16c593d5ff86530338668c80b78a56e8f1
loggerboy9325 <loggerboy9325@gmail.com>
2022-10-04 10:39:08 -0400

    testing

--- s3_files/script.js
+++ s3_files/script.js
@@ -1,9 +1,7 @@
-const API_URL =
-  "https://3w0rcv6178.execute-api.us-east-1.amazonaws.com/example/MyDemoResource";
-
 const getvisitors = async () => {
   try {
-    const url = API_URL;
+    const url =
+      "https://3w0rcv6178.execute-api.us-east-1.amazonaws.com/example/MyDemoResource";
     const res = await fetch(url);
     console.log(res.ok);
     const data = await res.json();
